a6072847c927859d9d9ace30c536e90ef2f23103
Alex Smith <alexsmith716@outlook.com>
2017-08-16 15:29:58 -0400

    Working on webpack file-loader & pug-loader for rendering pug to html. Progress made rendered html but still incomplete.

--- theServer/views/webpackPugEntry.js
+++ theServer/views/webpackPugEntry.js
@@ -1,6 +1,7 @@
 // Pug to HTML
 
 function requireAll(r) { 
+  console.log('>>>>>>>>>>>>>>> r.keys().forEach(r): ', r.keys().forEach(r))
   r.keys().forEach(r)
 }
 requireAll( require.context('./', true, /\.pug$/) )
