15ac2be2c43f84f18900c19f204d51ea6afbe227
Alex Smith <alexsmith716@outlook.com>
2017-08-20 11:57:06 -0400

    Ok, gulp, not webpack, is solution for uglify and concat pug and client side js. But hey, at the end of the day this is really a dev playground so going over webpack is a good thing. Now will try and rush through converting existing client js for an SPA.

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'appClient')))
+app.use(express.static(path.join(__dirname, 'appClientPublic')))
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* +++++++++++++++++++++++++++++++++++++++++++++++++ */
--- gulpFile.js
+++ gulpFile.js
@@ -1,28 +1,55 @@
 
-/*
-var gulp = require('gulp')
-var gulpSass = require('gulp-sass')
-var gulpHeader = require('gulp-header')
-var gulpCleanCss = require('gulp-clean-css')
-var gulpRename = require("gulp-rename")
-var gulpUglify = require('gulp-uglify')
-var browserSync = require('browser-sync').create()
-*/
-
 var gulp = require('gulp')
 var pug = require('gulp-pug')
 var watch = require('gulp-watch')
+var uglify = require('gulp-uglify')
+var concat = require('gulp-concat')
+var pump = require('pump')
+var fs = require('fs')
 
-gulp.task('pug',function() {
-  return gulp.src('./theServer/views/**/*.pug')
-  .pipe(pug({
-    doctype: 'html',
-    pretty: false
-  }))
-  .pipe(gulp.dest('./appClient/views'));
+// var webpack = require('webpack')
+// var webpackStream = require('webpack-stream')
+// var webpackConfig = require('./webpack.config.js')
+
+// var appClientFiles = fs.readdirSync('./appClient/js')
+
+gulp.task('clean', function () {
+  return del(['./appClientPublic'])
+})
+
+// errors are propagated forward through the piped streams
+// source streams are closed & destroyed if a destination stream closed
+// receive cause, file & line number of error
+
+gulp.task('compress', function (cb) {
+  pump([
+      gulp.src('./appClient/js/**/*.js'),
+      uglify({ compress : false }),
+      concat('all.min.js'),
+      gulp.dest('./appClientPublic/js')
+    ],
+    cb
+  )
+})
+
+gulp.task('pug',function (cb) {
+  pump([
+      gulp.src('./appClient/viewTemplates/**/*.pug'),
+      pug({
+        doctype: 'html',
+        pretty: false
+      }),
+      gulp.dest('./appClientPublic/views')
+    ],
+    cb
+  )
 })
 
 gulp.task('watch', function () {
-  return watch('./theServer/views/**/*.pug', { ignoreInitial: false })
-  .pipe(gulp.dest('pug'));
+  //return watch('./appClient/viewTemplates/**/*.pug', { ignoreInitial: false })
+  //.pipe(gulp.dest('pug'))
+  gulp.watch('./appClient/js/**/*.js', ['compress'])
+  gulp.watch('./appClient/viewTemplates/**/*.pug', ['pug'])
 })
+
+gulp.task('default', ['watch', 'compress', 'pug'])
